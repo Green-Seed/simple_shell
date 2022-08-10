a3c0f15b2b4257a5bd4ebe9cc7199154e3c9fc35
Green-Seed <ngubanethandiwe134@gmail.com>
2022-08-10 11:38:59 -0700

    print_env.c test

---
+++ test_shell/print_env.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+int main(int argc, char **argv, char **env)
+{
+	extern char **environ;
+
+	(void)argc;
+
+	(void)argv;
+
+	(void)env;
+
+	while (*environ != NULL)
+	{
+		printf("%s ", *environ);
+
+			environ++;
+	}
+
+	return (0);
+}
